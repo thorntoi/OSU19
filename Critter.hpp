aef8498df23f989795a3a6040cf0206963e338c8
hollow41 <dick.scott41@gmail.com>
2018-05-09 20:02:35 -0500

    Update Critter.hpp

--- Critter.hpp
+++ Critter.hpp
@@ -1,33 +1,54 @@
 /*------------------------------------------------------------------*
- * Critter.hpp
- * group Project
- *
- * Group 19 05/06/18
- *------------------------------------------------------------------*/
+* Critter.hpp
+* group Project
+*
+* Group 19 05/06/18
+*------------------------------------------------------------------*/
 #ifndef Critter_hpp
 #define Critter_hpp
-
 #include <iostream>
+using namespace std;
 
-class Critter{
+//not sure if direction will be needed for gameplay or not
+enum direction
+{
+	up, down, left, right
+};
+
+class Critter {
 private:
-    char species;
-    
+	char species;	// ant =='O' or doodlebug =='X'
+
 protected:
-    int rowPosition;
-    int colPostiion;
-    
+	int rowPosition;
+	int colPosition;
+	int step;			//number of moves ant/doodlebug surives
+	bool move;			//tracks if object passed through move()
+
 public:
-    //destructor
-    ~Critter();
-    
-    //methond to allow access for private species
-    void setSpecies(char);
-    
-    //move and breed virtual methods
-    virtual void move(int rowPos, int colPos);
-    virtual void breed();
-    
+	Critter();		//initializes move to true and step count to 0
+
+	int getRow();
+	int getCol();
+	int getStepCnt();		//tracks number of turns survived
+	bool getMove();
+	
+	void setCoord(int, int);	//may or may not need for setting coordinates
+
+	void setRow(int);
+	void setCol(int);
+	void setStepCnt(int);	//tracking number of moves for objects
+	void setSpecies(char);	// ant =='O' or doodlebug =='X'
+	void setMove(bool);		//number of moves ant/doodlebug surives
+
+
+	//virtual functions for ant & doodlebug
+	virtual void movement(Critter ***) = 0;
+	virtual void breed(Critter ***) = 0;
+
+	//destructor
+	~Critter();//
+	
 };
 
 
